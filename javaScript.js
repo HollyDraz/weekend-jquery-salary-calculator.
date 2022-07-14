0f3b9e16deed6a73189f70612814969122f5db77
Holly Drazenovich <hollydraze14@gmail.com>
2022-07-14 13:30:27 -0500

    added click handlerfor submit button and calls function propelry

--- javaScript.js
+++ javaScript.js
@@ -4,4 +4,11 @@ $(document).ready(readyNow);
 // ready now function for click holders
 function readyNow(){
     console.log('rock and roll!');
+    $('#submit-button').on('click', addEmployee );
+}
+
+// create function that adds employees to table on 'click'
+
+function addEmployee(){
+    console.log("added new employee");
 }
